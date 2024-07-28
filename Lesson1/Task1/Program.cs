129e57ac2c135caf7c26a58fe34ab140f2383cf0
kontinuum01 <kontinuum01@gmail.com>
2024-07-28 21:23:16 +0300

    add compare

--- Lesson1/Task1/Program.cs
+++ Lesson1/Task1/Program.cs
@@ -1,6 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 
 int value = 100;
 Console.WriteLine(value);
 
+int a = 5;
+int b = 8;
+
+if (a > b) {
+     Console.WriteLine(a);
+    }
+
+else {
+    Console.WriteLine(b);
+}
