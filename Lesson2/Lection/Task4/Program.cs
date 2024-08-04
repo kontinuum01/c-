d84ed1f6fa877af983f1bbc775cfe0712c43e57b
kontinuum01 <kontinuum01@gmail.com>
2024-08-04 23:21:54 +0300

    Добавили папки с задачами

---
+++ Lesson2/Lection/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если первое число некратно
+// второму, то программа выводит остаток от деления.
+// Примеры
+// 14, 5 => нет, 4
+// 16, 8 => да
+// 4, 3 => нет, 1
+
+Console.Write("Введите первое число: ");
+int firstValue = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondValue = Convert.ToInt32(Console.ReadLine());
+
+if(firstValue % secondValue == 0)
+{
+Console.WriteLine($"{firstValue}, {secondValue} => Да");
+}
+else
+{
+Console.WriteLine($"{firstValue}, {secondValue} => Нет, {firstValue % secondValue}");
+}
