257d19c9c544e9f8e7811819a1be28363f4f5048
kontinuum01 <kontinuum01@gmail.com>
2024-08-05 20:46:37 +0300

    Добавили задачу 1

--- Lesson2/Seminar/Task1/Program.cs
+++ Lesson2/Seminar/Task1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает 
+//на вход число и проверяет, кратно ли оно 
+//одновременно 7 и 23
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if( number % 7 == 0 && number % 23 == 0)
+{
+Console.WriteLine($"{number} => Да");
+}
+else
+{
+Console.WriteLine($"{number} => Нет");
+}
