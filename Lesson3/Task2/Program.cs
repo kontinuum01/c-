67064f1451b38edc62d3a93875f7f0e767f5c238
kontinuum01 <kontinuum01@gmail.com>
2024-08-11 16:24:02 +0300

    Добавили задачи по циклам

---
+++ Lesson3/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив из 10 элементов, заполненный числами из
+// промежутка [-10, 10]. Замените отрицательные элементы на
+// положительные, а положительные на отрицательные.
+// Пример
+// [1 -5 6]
+// => [-1 5 -6]
+
+int[] numbers = {1, -5, 6, 6, 3, 9, -7};
+
+foreach(int number in numbers)
+{
+Console.Write(number + "\t");
+}
+System.Console.WriteLine();
+
+for(int i = 0; i < numbers.Length; i++)
+{
+numbers[i] *= -1;
+}
+
+foreach(int number in numbers)
+{
+System.Console.Write(number + "\t");
+}
---
+++ Lesson3/Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿/ Найдите произведения пар чисел в одномерном массиве. Парой
+// считаем первый и последний элемент, второй и предпоследний и
+// т.д. Результат запишите в новый массив.
+// Пример
+// [1 3 2 4 2 3] => [3 6 8]
+// [2 3 1 7 5 6 3] => [6 18 5] (элемент 7 не имеет пары)
+
+int[] numbers = { 1, -5, 6, 6, 3, 9, -7, 4, 2, 5, 1, 2 };
+
+foreach (int number in numbers)
+{
+Console.Write(number + "\t");
+}
+System.Console.WriteLine();
+
+int[] multPairs = new int[numbers.Length / 2];
+
+// for (int i = 0; i < multPairs.Length; i++)
+// {
+// multPairs[i] = numbers[i] * numbers[numbers.Length - 1 - i];
+// }
+
+for (int i = 0, j = numbers.Length - 1; i < multPairs.Length; i++, j--)
+{
+multPairs[i] = numbers[i] * numbers[j];
+}
+
+foreach (int pair in multPairs)
+{
+Console.Write(pair + "\t");
+}
