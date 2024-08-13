5c4f5b70025c1952451c5612a3bf1ffec0a8b260
kontinuum01 <kontinuum01@gmail.com>
2024-08-13 22:18:15 +0300

    Добавили лекцию 4

---
+++ Lesson4/Lection/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Вычислить значение формулы a*b/c+d. Решение оформить в виде функции
+
+//Метод
+double CalculateFormula(int a, int b, int c, int d) 
+{
+    double numenator = a * b;
+    int domenator = c + d;
+    double result = numenator / domenator;
+    return result;
+}
+//Вызов метода(функции) с передаваемыми аргументами
+double result = CalculateFormula(1, 2,, 3, 4);
+Console.WriteLine("Hello, World!");
